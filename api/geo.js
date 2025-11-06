2b73ca5692aa3912dc1aae7156b5d67884144c04
Tangent0777 <132768517+Tangent0777@users.noreply.github.com>
2025-11-06 15:10:45 +0530

    Update geo.js

--- api/geo.js
+++ api/geo.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   try {
-    const geoRes = await fetch("https://ipwho.org/json/");
+    const geoRes = await fetch("https://api.ipwho.org/?lang=en");
     const data = await geoRes.json();
     res.status(200).json(data);
   } catch (err) {
